e2ef48e535ad8b6d3a2e552e333a0c2bf8f2bf97
eytangut <eytangut@gmail.com>
2024-06-21 09:14:08 +0000

    updated db

--- createtable.sql
+++ createtable.sql
@@ -2,7 +2,7 @@ CREATE TABLE mail (
     mail TEXT NOT NULL,
     user_id INTEGER,
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 
 );
 
