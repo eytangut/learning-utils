5a15262b027e6ec8e9dd2852edaeeb48d39db1bd
eytangut <eytangut@gmail.com>
2024-06-22 19:10:44 +0000

    modified:   test/script.py

--- test/script.py
+++ test/script.py
@@ -2,4 +2,12 @@ import os
 import google.generativeai as genai
 
 GOOGLE_API_KEY = ''
-print(GOOGLE_API_KEY)
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+while True:
+    x = input()
+    if x != 'quit':
+        print(chat.send_message(x).text)
+    else:
+        break 
